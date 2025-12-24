6428c1bc6a95c8ded5c13ed53523f9d5f7cad153
Andrei Romm <andrew.romm@gmail.com>
2025-12-24 17:42:51 +0300

    step3

--- src/FilterCafes.jsx
+++ src/FilterCafes.jsx
@@ -1,7 +1,18 @@
+const subways = [
+  { name: 'Арбатская', code: 'Arbat' },
+  { name: 'Александровский сад', code: 'Alexanders Garden' },
+  { name: 'Московская', code: 'Moscow' },
+  { name: 'Парк Культуры', code: 'Culture' },
+  { name: 'Театральная', code: 'Theater' },
+];
+
 const FilterCafes = () => (
   <div className="controls">
     <select name="subway" id="subway">
       <option value="All">Все</option>
+      {subways.map((subway) => (
+        <option key={subway.code} value={subway.code}>{subway.name}</option>
+      ))}
     </select>
   </div>
 );
